e9cd10a98b777e264a8595d37bb94e44e2445ab5
Ruchith Keerala <ruchith.k@nextrow.com>
2024-04-29 11:48:56 +0530

    Deleted

--- BookMyShow/admin/src/main/java/com/bookmyshow/admin/DataObjects/ShowRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookmyshow.admin.DataObjects;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShowRepository extends JpaRepository<ShowEntity,String> {
-}
--- BookMyShow/show/src/main/java/com/bookmyshow/show/repository/ShowRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookmyshow.show.repository;
-
-import com.bookmyshow.show.entity.ShowEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShowRepository extends JpaRepository<ShowEntity,String> {
-}
